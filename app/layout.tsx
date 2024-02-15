72fbf9de90041630c4856a50bb1b32daab95dd90
Jumaru <spiralout.eu@gmail.com>
2024-02-16 00:58:18 +0200

    rename title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dollar Bridge",
+  title: "WhiteBridge",
   description: "Bridge your USDC on the other side",
 };
 
