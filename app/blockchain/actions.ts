4b104dc88d18d634ce568281e6fcf4049ba09705
Jumaru <spiralout.eu@gmail.com>
2024-02-16 21:29:59 +0200

    add deposit for burn

--- app/blockchain/actions.ts
+++ app/blockchain/actions.ts
@@ -1,5 +1,7 @@
 import { BigNumber, ethers } from "ethers";
 import erc20abi from "./contracts/erc20.abi.json";
+import tokenMessengerAbi from "./contracts/tokenMessenger.abi.json";
+import { addressToBytes32 } from "./utils";
 
 // Function to approve token transfer
 export async function approveTokenTransfer(
@@ -14,6 +16,36 @@ export async function approveTokenTransfer(
     const tx = await tokenContract
       .connect(signer)
       .approve(spenderAddress, amount);
+    await tx.wait();
+    return tx.hash;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function depositForBurn(
+  tokenMessengerAddress: string,
+  amount: BigNumber,
+  destinationDomain: number,
+  destinationAddress: string,
+  tokenAddress: string,
+  signer: ethers.Signer
+) {
+  const tokenMessengerContract = new ethers.Contract(
+    tokenMessengerAddress,
+    tokenMessengerAbi,
+    signer
+  );
+
+  try {
+    const tx = await tokenMessengerContract
+      .connect(signer)
+      .depositForBurn(
+        amount,
+        destinationDomain,
+        addressToBytes32(destinationAddress),
+        tokenAddress
+      );
     await tx.wait();
     return tx.hash;
   } catch (error) {
