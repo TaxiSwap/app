18dd92508f976ae9225998f188c8f08f3ababd87
Jumaru <spiralout.eu@gmail.com>
2024-02-22 18:55:02 +0200

    add api to get addressToBytes32

---
+++ app/api/blockchain/utils/addressToBytes32/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { addressToBytes32 } from "../../../../blockchain/utils";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const address = url.searchParams.get("address");
+
+  try {
+    if (!address) throw new Error("Address is required");
+    const result = addressToBytes32(address);
+    return NextResponse.json({ result });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
