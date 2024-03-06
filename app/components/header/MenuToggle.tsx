da6bc9e5800c69f93a554e76a5b62ffc0e329bf5
Jumaru <spiralout.eu@gmail.com>
2024-03-06 23:42:46 +0200

    refactor: :lipstick: change svg icons to material design standard icons

--- app/components/header/MenuToggle.tsx
+++ app/components/header/MenuToggle.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import { MdMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
 
 interface MenuToggleProps {
   isMenuOpen: boolean;
@@ -14,31 +16,13 @@ const MenuToggle: React.FC<MenuToggleProps> = ({ isMenuOpen, toggleMenu }) => {
       aria-expanded={isMenuOpen}
       aria-label="Toggle menu"
     >
-      <svg
-        className="w-6 h-6"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {isMenuOpen ? (
-          // Icon for when the menu is open (e.g., an 'X' icon)
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        ) : (
-          // Hamburger icon for when the menu is closed
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        )}
-      </svg>
+      {isMenuOpen ? (
+        // 'X' Icon for when the menu is open 
+        <MdClose className="text-3xl font-bold" />
+      ) : (
+        // 'Hamburger' icon for when the menu is closed
+        <MdMenu className="text-3xl font-bold" />
+      )}
     </button>
   );
 };
