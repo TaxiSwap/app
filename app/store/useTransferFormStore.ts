e461be999c3f25eaaf6a4f4bb18012a72ac1fc4d
Jumaru <spiralout.eu@gmail.com>
2024-03-08 13:00:41 +0200

    feat: :sparkles: add store to handle transfer form state

---
+++ app/store/useTransferFormStore.ts
@@ -0,0 +1,44 @@
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware';
+import { useNetworkStore } from './useNetworkConfig';
+
+// Define a type for the store's state
+interface TransferFormState {
+  sourceChain: string;
+  destinationChain: string;
+  destinationAddress: string;
+  amount: number;
+  userBalance: number;
+  isAddressValid: boolean;
+  isAmountValid: boolean;
+  setSourceChain: (chain: string) => void;
+  setDestinationChain: (chain: string) => void;
+  setDestinationAddress: (address: string) => void;
+  setAmount: (amount: number) => void;
+  setUserBalance: (balance: number) => void;
+  setIsAddressValid: (isValid: boolean) => void;
+  setIsAmountValid: (isValid: boolean) => void;
+}
+
+// Initialize the store with state and actions
+export const useTransferFormStore = create<TransferFormState>()(
+  devtools((set) => ({
+    // Initial state
+    sourceChain: '',
+    destinationChain: '',
+    destinationAddress: '',
+    amount: 0,
+    userBalance: 0,
+    isAddressValid: false,
+    isAmountValid: false,
+    // Actions to update state
+    setSourceChain: (chain) => set(() => ({ sourceChain: chain })),
+    setDestinationChain: (chain) => set(() => ({ destinationChain: chain })),
+    setDestinationAddress: (address) => set(() => ({ destinationAddress: address })),
+    setAmount: (amount) => set(() => ({ amount })),
+    setUserBalance: (balance) => set(() => ({ userBalance: balance })),
+    setIsAddressValid: (isValid) => set(() => ({ isAddressValid: isValid })),
+    setIsAmountValid: (isValid) => set(() => ({ isAmountValid: isValid })),
+  })),
+);
+
