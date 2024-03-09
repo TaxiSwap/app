81a558f591b946e929cdc311e8fc32850788e393
Jumaru <spiralout.eu@gmail.com>
2024-03-09 18:03:02 +0200

    feat: :sparkles: add transfer header component

---
+++ app/components/transferForm/TransferHeader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TransferHeader: React.FC = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold mb-4">Transfer USDC across chains</h2>
+      <p className="mb-8 text-gray-600">
+        Our USDC bridge simplifies cross-network transactions, offering a
+        cost-effective and straightforward solution for transferring your
+        digital currency. Designed for ease and efficiency, it ensures seamless
+        and affordable transfers across chains, making it the go-to choice for
+        managing your USDC assets.
+      </p>
+    </>
+  );
+};
+
+export default TransferHeader;
