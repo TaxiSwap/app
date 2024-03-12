1fa4b0a86714f66833b8a7c9470b80d195ac280c
Jumaru <spiralout.eu@gmail.com>
2024-03-12 18:44:09 +0200

    refactor: :wastebasket: remove responsibility for additional form data

--- app/store/useTransferFormStore.ts
+++ app/store/useTransferFormStore.ts
@@ -7,16 +7,10 @@ interface TransferFormState {
   destinationChain: string;
   destinationAddress: string;
   amount: number;
-  userBalance: number;
-  isAddressValid: boolean;
-  isAmountValid: boolean;
   setSourceChain: (chain: string) => void;
   setDestinationChain: (chain: string) => void;
   setDestinationAddress: (address: string) => void;
   setAmount: (amount: number) => void;
-  setUserBalance: (balance: number) => void;
-  setIsAddressValid: (isValid: boolean) => void;
-  setIsAmountValid: (isValid: boolean) => void;
 }
 
 // Initialize the store with state and actions
@@ -27,17 +21,11 @@ export const useTransferFormStore = create<TransferFormState>()(
     destinationChain: '',
     destinationAddress: '',
     amount: 0,
-    userBalance: 0,
-    isAddressValid: false,
-    isAmountValid: false,
     // Actions to update state
     setSourceChain: (chain) => set(() => ({ sourceChain: chain })),
     setDestinationChain: (chain) => set(() => ({ destinationChain: chain })),
     setDestinationAddress: (address) => set(() => ({ destinationAddress: address })),
     setAmount: (amount) => set(() => ({ amount })),
-    setUserBalance: (balance) => set(() => ({ userBalance: balance })),
-    setIsAddressValid: (isValid) => set(() => ({ isAddressValid: isValid })),
-    setIsAmountValid: (isValid) => set(() => ({ isAmountValid: isValid })),
   })),
 );
 
