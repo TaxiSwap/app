32fcc216d91b905654a8a1394b665ecde2838ae8
Jumaru <spiralout.eu@gmail.com>
2024-03-13 10:14:22 +0200

    style: :lipstick: add logo on brand

--- app/components/header/Brand.tsx
+++ app/components/header/Brand.tsx
@@ -1,10 +1,11 @@
-// Brand.tsx
 import React from 'react';
+import { TbBuildingBridge } from 'react-icons/tb';
 
 const Brand: React.FC = () => {
   return (
-    <div className="text-gray-800 text-2xl font-bold">
-      WhiteBridge
+    <div className="flex items-center text-gray-800 text-2xl font-bold">
+      <TbBuildingBridge className="mr-2 text-4xl" />
+      WHITEBRIDGE
     </div>
   );
 };
