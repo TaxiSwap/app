8f359b8a8611be8fb82bf2299b9133a5ef1a7492
Jumaru <spiralout.eu@gmail.com>
2024-03-14 19:37:27 +0200

    style: :lipstick: rebranding

--- app/components/header/Brand.tsx
+++ app/components/header/Brand.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { TbBuildingBridge } from 'react-icons/tb';
+import React from "react";
+import { BsTaxiFront } from "react-icons/bs";
 
 const Brand: React.FC = () => {
   return (
     <div className="flex items-center text-gray-800 text-2xl font-bold">
-      <TbBuildingBridge className="mr-2 text-4xl" />
-      WHITEBRIDGE
+      <BsTaxiFront className="mr-2 text-4xl" />
+      <div
+        className="bg-gray-50 p-4 max-w-sm mx-auto rounded-lg shadow-md flex justify-center items-center space-x-2"
+        style={{ border: "1px solid #999" }}
+      >
+        <span className="text-2xl font-semibold tracking-widest text-gray-800">
+          TAXISWAP.xyz
+        </span>
+      </div>
     </div>
   );
 };
