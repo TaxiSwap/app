0e3a29764a138bf9ede6a841aeee31d3ac427343
Jumaru <spiralout.eu@gmail.com>
2024-03-16 20:03:22 +0200

    style: :speech_balloon: change header text

--- app/components/transferForm/TransferHeader.tsx
+++ app/components/transferForm/TransferHeader.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const TransferHeader: React.FC = () => {
   return (
     <>
       <h2 className="text-3xl font-bold mb-4">Transfer USDC across chains</h2>
       <p className="mb-8 text-gray-600">
-        Our USDC bridge simplifies cross-network transactions, offering a
-        cost-effective and straightforward solution for transferring your
-        digital currency. Designed for ease and efficiency, it ensures seamless
-        and affordable transfers across chains, making it the go-to choice for
-        managing your USDC assets.
+        TaxiSwap&apos;s USDC bridge streamlines transfers between networks, offering
+        an affordable and intuitive way to move your digital currency. Crafted
+        for ease and efficiency, it ensures smooth and cost-effective
+        transactions across an expanding array of chains. As we continuously
+        integrate more networks, tokens, and innovative features, TaxiSwap is
+        setting the standard for seamless management of your USDC assets in the
+        ever-evolving DeFi landscape.
       </p>
     </>
   );
