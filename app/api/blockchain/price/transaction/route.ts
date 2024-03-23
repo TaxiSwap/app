cc8bc8b002f562da93a919db12e3689e499e8544
Jumaru <spiralout.eu@gmail.com>
2024-03-23 20:41:54 +0200

    feat: :sparkles: add check for transaction type input

--- app/api/blockchain/price/transaction/route.ts
+++ app/api/blockchain/price/transaction/route.ts
@@ -7,12 +7,13 @@ import { TokenPriceRepository } from "@/app/repositories/TokenPriceRepository";
 import { CoinGeckoStrategy } from "@/app/services/tokenPriceFetching/CoinGeckoStrategy";
 import { CoinMarketCapStrategy } from "@/app/services/tokenPriceFetching/CoinMarketCapStrategy";
 import { EvmGasPriceFetchingStrategy } from "@/app/services/gasPriceFetching/EvmGasPriceFetchingStrategy";
+import { GasAmounts } from "@/app/config/ChainConfigMap";
 
 // Ensure BigInt can be serialized in JSON responses
 (BigInt.prototype as any).toJSON = function () {
-    return this.toString();
-  };
-  
+  return this.toString();
+};
+
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const chainId = url.searchParams.get("chainId") || undefined;
@@ -24,7 +25,11 @@ export async function GET(req: Request) {
       { status: 400 }
     );
   }
+  const validKeys: (keyof GasAmounts)[] = ["receive", "approve", "deposit"];
 
+  if (!validKeys.includes(transactionType as keyof GasAmounts)) {
+    throw new Error(`"${transactionType}" is not a valid key of GasAmounts.`);
+  }
   const gasPriceService = new GasPriceService(
     new GasPriceRepository(),
     new EvmGasPriceFetchingStrategy()
@@ -41,7 +46,7 @@ export async function GET(req: Request) {
 
   try {
     const costDetails = await transactionCostService.calculateTransactionCost(
-      transactionType,
+      transactionType as keyof GasAmounts,
       chainId
     );
     if (costDetails) {
@@ -53,10 +58,9 @@ export async function GET(req: Request) {
       );
     }
   } catch (error) {
-    console.error("Error calculating transaction costs:", error);
-    return NextResponse.json(
-      { error: "Unexpected error calculating transaction costs." },
-      { status: 500 }
-    );
+    const errorMessage =
+      error instanceof Error ? error.message : "An unexpected error occurred";
+    console.error("Error calculating transaction costs:", errorMessage);
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
