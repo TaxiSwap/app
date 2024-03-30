9b6f5896a28da93194804877a43c885816571b7e
Jumaru <spiralout.eu@gmail.com>
2024-03-30 23:45:52 +0200

    feat: :sparkles: show testnet only when env testnet is set

--- app/components/header/MenuItems.tsx
+++ app/components/header/MenuItems.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import NetworkToggle from './NetworkToggle';
-import NetworkButton from './NetworkButton';
-import WalletButton from './WalletButton';
+import React from "react";
+import NetworkToggle from "./NetworkToggle";
+import NetworkButton from "./NetworkButton";
+import WalletButton from "./WalletButton";
+import useEnvFlags from "@/app/hooks/useEnvFlags";
 
 // Define an interface for the component's props
 interface MenuItemsProps {
   isMenuOpen: boolean; // Indicates whether the mobile menu is open or closed
-  displayMode: 'mobile' | 'desktop'; // Restricts the display mode to either 'mobile' or 'desktop'
+  displayMode: "mobile" | "desktop"; // Restricts the display mode to either 'mobile' or 'desktop'
 }
 
 const MenuItems: React.FC<MenuItemsProps> = ({ isMenuOpen, displayMode }) => {
   // Define class names based on display mode for responsive design
-  const containerClassNames = displayMode === 'mobile'
-    ? `absolute bg-white p-4 right-0 mt-2 w-auto md:hidden ${isMenuOpen ? 'flex' : 'hidden'} flex-col items-end space-y-3`
-    : 'hidden md:flex items-center space-x-3';
+  const containerClassNames =
+    displayMode === "mobile"
+      ? `absolute bg-white p-4 right-0 mt-2 w-auto md:hidden ${
+          isMenuOpen ? "flex" : "hidden"
+        } flex-col items-end space-y-3`
+      : "hidden md:flex items-center space-x-3";
+  const { isTestnet, isLoading } = useEnvFlags();
 
   return (
     <div className={containerClassNames}>
-      {process.env.ADMIN_FLAG === 'true' && <NetworkToggle />}
+      {isTestnet && !isLoading && <NetworkToggle />}
       <NetworkButton />
       <WalletButton />
     </div>
