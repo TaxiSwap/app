9d5d306bd15927e41a432cb6dfe0d6d70b3e8762
Jumaru <spiralout.eu@gmail.com>
2024-03-31 22:14:38 +0300

    style: :speech_balloon: change display text of submit button to `Go!`

--- app/components/transferForm/TransferForm.tsx
+++ app/components/transferForm/TransferForm.tsx
@@ -112,7 +112,7 @@ const TransferForm = () => {
         />
 
         <button
-          className={`w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none shadow transition duration-300
+          className={`w-full text-white font-bold rounded-lg text-xl px-5 py-2.5 focus:outline-none shadow transition duration-300
               ${
                 !isTransferValid
                   ? "bg-gray-400 hover:bg-gray-400 cursor-not-allowed"
@@ -120,7 +120,7 @@ const TransferForm = () => {
               }`}
           disabled={!isTransferValid}
         >
-          NEXT
+          Go!
         </button>
         <TransactionSummary
           gasCost={isSourceNetworkAsWallet ? estimatedUserGasCostInEther: null}
