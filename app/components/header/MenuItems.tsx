f01d7427a0787bff13df5da956b10a8cb2dc8b5b
Jumaru <spiralout.eu@gmail.com>
2024-06-01 00:23:48 +0300

    fix z index of menu items on mobile view

--- app/components/header/MenuItems.tsx
+++ app/components/header/MenuItems.tsx
@@ -14,7 +14,7 @@ const MenuItems: React.FC<MenuItemsProps> = ({ isMenuOpen, displayMode }) => {
   // Define class names based on display mode for responsive design
   const containerClassNames =
     displayMode === "mobile"
-      ? `absolute bg-header-yellow shadow-8 border-2 border-gray-200 rounded-lg p-4 right-0 mt-2 w-auto md:hidden ${
+      ? `absolute bg-header-yellow shadow-8 border-2 border-gray-200 rounded-lg p-4 right-0 mt-2 w-auto md:hidden z-40 ${
           isMenuOpen ? "flex" : "hidden"
         } flex-col items-end space-y-3`
       : "hidden md:flex items-center space-x-3";
