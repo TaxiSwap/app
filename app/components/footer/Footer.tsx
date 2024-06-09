4a1759fecf4e0943cfa2264a72dfd8fe2823118d
Jumaru <spiralout.eu@gmail.com>
2024-06-09 15:54:55 +0300

    update footer design

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -5,9 +5,9 @@ import FooterLink from "./FooterLink";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white p-5 shadow-t mt-14">
+    <footer className="bg-blackish p-5 shadow-t mt-4">
       <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between text-center sm:text-left">
-        <p className="text-sm text-gray-600 mb-4 sm:mb-0">
+        <p className="text-sm text-header-yellow mb-4 sm:mb-0">
          <> © {new Date().getFullYear()} Taxiswap.xyz . All rights reserved.</>
         </p>
         <div className="flex justify-center sm:justify-between flex-wrap space-x-4">
--- app/components/footer/FooterLink.tsx
+++ app/components/footer/FooterLink.tsx
@@ -11,8 +11,8 @@ interface FooterLinkProps {
 const FooterLink: React.FC<FooterLinkProps> = ({ href, label, icon: Icon }) => {
   return (
     <Link href={href} passHref> 
-      <div className="flex items-center space-x-2 text-gray-600 hover:text-gray-900 transition-colors duration-200">
-        <Icon size={20} />
+      <div className="flex items-center space-x-2 text-header-yellow hover:font-bold transition-colors duration-200">
+        <Icon size={20}/>
         <span>{label}</span>
       </div>
     </Link>
